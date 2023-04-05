dc52ef42d2607526680db885226248d95ca5487e
doozles411 <cindy.phelps01@gmail.com>
2023-04-04 19:18:55 -0700

    Add text

--- src/components/Left.js
+++ src/components/Left.js
@@ -5,7 +5,7 @@ function Left() {
         <div className='left'>
             <h2>Lessons</h2>
             <div className='left-content'>
-                <p>Many styles to choose from:</p>
+                <p className='list-descriptor'>Many styles to choose from:</p>
                 <ul>
                     <li>Salsa</li>
                     <li>Tango</li>
@@ -17,6 +17,18 @@ function Left() {
                     <li>Yoga</li>
                     <li>General partner work</li>
                 </ul>
+                <p className='lesson-text'>Lessons are tailored to your needs, whether that is learning a new style, improving your current skills, or preparing for a special event.</p>
+                <p className='lesson-text'>If you don't have a particular dance in mind, I'm happy to recommend styles that will best meet your needs.</p>
+                <p className='lesson-text'>Our first lesson will take place at <a href='https://www.dancecenterchicago.com'>Dance Center Chicago</a>:
+                    <br/>
+                    3868 N Lincoln Ave.
+                    <br/>
+                    2nd Floor
+                    <br/>
+                    Chicago, IL 60613
+                </p>
+                <p className='lesson-text'></p>
+                <p className='lesson-text'></p>
             </div>
         </div>
     );
