cc17a0a74cf3e602ad4fc19c25d278c9e189e06f
doozles411 <cindy.phelps01@gmail.com>
2023-04-05 12:53:07 -0700

    Add contact div

--- src/components/Right.js
+++ src/components/Right.js
@@ -4,6 +4,9 @@ function Right() {
     return (
         <div className='right'>
             <h2>About</h2>
+            <div className='contact'>
+                <p>Call (206) 240-4372 to schedule a lesson</p>
+            </div>
         </div>
     );
 }
