d69bd057fb3632183da245f0fa2c033b5de20e2f
doozles411 <cindy.phelps01@gmail.com>
2023-04-04 18:47:36 -0700

    Create lesson list

--- src/components/Left.js
+++ src/components/Left.js
@@ -4,6 +4,20 @@ function Left() {
     return (
         <div className='left'>
             <h2>Lessons</h2>
+            <div className='left-content'>
+                <p>Many styles to choose from:</p>
+                <ul>
+                    <li>Salsa</li>
+                    <li>Tango</li>
+                    <li>Bachata</li>
+                    <li>Merengue</li>
+                    <li>Swing</li>
+                    <li>Polka</li>
+                    <li>Waltz</li>
+                    <li>General partner work (fusion)</li>
+                    <li>Yoga</li>
+                </ul>
+            </div>
         </div>
     );
 }
