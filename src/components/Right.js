42d6965be1bb46f13f27c62725f4e5db03122f7d
doozles411 <cindy.phelps01@gmail.com>
2023-04-05 13:11:32 -0700

    Add span for phone

--- src/components/Right.js
+++ src/components/Right.js
@@ -6,7 +6,7 @@ function Right() {
         <div className='right'>
             <h2>About</h2>
             <div className='contact'>
-                <p>Call (206) 240-4372 to schedule a lesson</p>
+                <p>Call <span>(206) 240-4372</span> to schedule a lesson</p>
                 <a
                     href='https://www.instagram.com/cateloose'
                     target="_blank"
