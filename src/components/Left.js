aa82b92046669f296c463d3e99cf559c66139458
doozles411 <cindy.phelps01@gmail.com>
2023-04-04 19:28:18 -0700

    Add text

--- src/components/Left.js
+++ src/components/Left.js
@@ -19,16 +19,25 @@ function Left() {
                 </ul>
                 <p className='lesson-text'>Lessons are tailored to your needs, whether that is learning a new style, improving your current skills, or preparing for a special event.</p>
                 <p className='lesson-text'>If you don't have a particular dance in mind, I'm happy to recommend styles that will best meet your needs.</p>
-                <p className='lesson-text'>Our first lesson will take place at <a href='https://www.dancecenterchicago.com'>Dance Center Chicago</a>:
-                    <br/>
+                <p className='lesson-text'>
+                    Our first lesson will take place at 
+                    <a 
+                        href='https://www.dancecenterchicago.com'
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Dance Center Chicago
+                    </a>
+                    :
+                </p>
+                <p className='lesson-text address'>
                     3868 N Lincoln Ave.
                     <br/>
                     2nd Floor
                     <br/>
-                    Chicago, IL 60613
-                </p>
-                <p className='lesson-text'></p>
-                <p className='lesson-text'></p>
+                    Chicago, IL 60613</p>
+                <p className='lesson-text'>All following lessons can take place in home or at an agreed upon location.</p>
+                <p className='lesson-text'>My rate is $60 per hour for singles and couples.</p>
             </div>
         </div>
     );
