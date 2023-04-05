e9ea730df293ae97af8e17e5f4fac42052307681
doozles411 <cindy.phelps01@gmail.com>
2023-04-04 17:40:58 -0700

    Import and render main image

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import smile from '../assets/images/smile.png'
 
 function Main() {
     return (
         <div className='main'>
-            <h1>Cate Curtis</h1>
+            <div className='header'>
+                <h1>Cate Curtis</h1>
+                <div className='images'>
+                    <img 
+                        alt='Partnered dancing'
+                        src={smile}
+                    />
+                </div>
+            </div>
         </div>
     );
 }
